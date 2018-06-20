e608ca3cb0f29526b116c0041b08de7652bddfa9
dc <429544557@qq.com>
2018-06-20 14:07:44 +0800

    commit

--- src/main/java/com/dc/demo/SpringBootDemoApplication.java
+++ src/main/java/com/dc/demo/SpringBootDemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * drools基础教程：
  * 		https://github.com/MyHerux/drools-springboot
  * 		Drools5&jBPM5详尽教程（自学笔记整理）.docx
+ * drools fusion
+ * 		Drools-Fusion-用户指南-中文.pdf
  * drools实际使用案例
  * 		安全技术架构说明书.pptx
  * @author dc
