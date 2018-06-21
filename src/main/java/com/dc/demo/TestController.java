e769742dc7e23f9b0abe590d40f4fb222632dbde
beijing-penguin <429544557@qq.com>
2018-06-21 18:31:52 +0800

    commit

--- src/main/java/com/dc/demo/TestController.java
+++ src/main/java/com/dc/demo/TestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dc.demo.entity.Person;
+import com.dc.demo.service.TestService;
 
 /**
  * 
@@ -17,11 +18,15 @@ public class TestController {
 	
 	@Autowired
 	private StatelessKieSession kieSession;
+	@Autowired
+	private TestService testService;
 	
 	@RequestMapping("/test1")
 	public Person test1() {
 		Person p = new Person();
 		p.setAge(31);
+		
+		kieSession.setGlobal("testService", testService);
 		kieSession.execute(p);
 		return p;
 	}
